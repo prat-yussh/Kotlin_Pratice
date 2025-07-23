3743d06d52de5cc029a7958f6089a92411fd07c3
pratyush3112 <pratyushkumar3m@gmail.com>
2025-07-23 10:04:48 +0530

    1

--- hello.kt
+++ hello.kt
@@ -1,3 +1,19 @@
-fun main() {
-    println("Hello from VS Code!")
+abstract class Animal {
+    abstract fun Eating()
+
+    fun sleep() {
+        println("Sleeping")
+    }
+}
+
+class Dog : Animal() {
+    override fun Eating() {
+        println("Eat")
+    }
+}
+
+fun main(args: Array<String>) {
+    val Dog1 = Dog()
+    Dog1.Eating()
+    Dog1.sleep()
 }
